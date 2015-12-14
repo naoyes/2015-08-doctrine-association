4e381061172fa1527a5c373fdebaf106e8ef132d
Naoyasu MANABE <n-manabe@quartetcom.co.jp>
2015-12-14 09:53:30 +0900

    1.3 One-To-One > Self Referencing > Student. `./vendor/bin/doctrine orm:generate:entities ./src`

--- src/OneToOne/SelfReferencing/Student.php
+++ src/OneToOne/SelfReferencing/Student.php
@@ -25,4 +25,37 @@ class Student
      * @JoinColumn(name="mentor_id", referencedColumnName="id")
      */
     private $mentor;
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set mentor
+     *
+     * @param \OneToOne\SelfReferencing\Student $mentor
+     * @return Student
+     */
+    public function setMentor(\OneToOne\SelfReferencing\Student $mentor = null)
+    {
+        $this->mentor = $mentor;
+
+        return $this;
+    }
+
+    /**
+     * Get mentor
+     *
+     * @return \OneToOne\SelfReferencing\Student
+     */
+    public function getMentor()
+    {
+        return $this->mentor;
+    }
 }
